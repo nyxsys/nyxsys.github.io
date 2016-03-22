18deab9980e46b17465a40cbdf3149fea1768605
nyxsys <isaprettycoolguy@gmail.com>
2016-03-22 16:11:24 -0400

    actually tracked removed items this time

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('jaimino', [
+angular.module('mrbarbera', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
--- components/projects/projects.service.js
+++ components/projects/projects.service.js
@@ -14,40 +14,6 @@ angular.module('mrbarbera')
     };
     var projects = [
         new project(
-            "Financial Advisor App", 
-            "https://github.com/jaaimino/financial-advisor-app", 
-            "assets/img/project_thumbs/finadvise_thumb.png", "_blank"
-        ),
-        new project(
-            "Tastimals", 
-            "https://github.com/jaaimino/tastimals", 
-            "assets/img/project_thumbs/tastimals_thumb.png", "_blank"
-        ),
-        new project(
-            "Dogebook", 
-            "https://github.com/jaaimino/dogebook", 
-            "assets/img/project_thumbs/dogebook_thumb.png", 
-            "_blank"
-        ),
-        new project(
-            "Gemini Simulator", 
-            "https://github.com/jaaimino/gemini-simulator", 
-            "assets/img/project_thumbs/gemini_thumb.png", 
-            "_blank"
-        ),
-        new project(
-            "Jaimino.com", 
-            "https://github.com/jaaimino/jaaimino.github.io", 
-            "assets/img/project_thumbs/jaimino_thumb.png", 
-            "_blank"
-        ),
-        new project(
-            "Reddit Downloader", 
-            "https://github.com/jaaimino/reddit-downloader", 
-            "assets/img/project_thumbs/redditd_thumb.png", 
-            "_blank"
-        ),
-        new project(
             "JSDungeon", 
             "https://github.com/jaaimino/jsdungeon", 
             "assets/img/project_thumbs/jsdungeon_thumb.png", 
