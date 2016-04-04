41bea97936781d0465a7b81a685626e609eb3a37
jaaimino <jacobaimino@gmail.com>
2016-04-04 12:08:01 -0400

    Added offcanvas

--- app/main/main.controller.js
+++ app/main/main.controller.js
@@ -2,5 +2,9 @@
 
 angular.module('mrbarbera')
   .controller('MainCtrl', function ($scope, $http) {
-
+    $(document).ready(function () {
+      $('[data-toggle="offcanvas"]').click(function () {
+        $('.row-offcanvas').toggleClass('active')
+      });
+    });
   });
